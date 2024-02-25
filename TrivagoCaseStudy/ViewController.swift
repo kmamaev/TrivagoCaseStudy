57d2f04ccaeb1de6a544e74642498c52d6e7f3e5
Konstantin Mamaev <konstantin.mamaev@picsart.com>
2024-02-25 14:59:43 +0100

    Initial Commit

---
+++ TrivagoCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrivagoCaseStudy
+//
+//  Created by Konstantin Mamaev on 25.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
